b9fd0a3325347a96d1e2bb264e38e04362ee3bd8
Suzuki Kazuto <suzuki.kazuto@fujits.com>
2023-12-08 19:39:14 +0900

    styling_1st

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,52 @@
 // App.tsx
 import React from "react";
+import { AppBar, Chip, Tooltip } from "@mui/material";
 import CharacterSearch from "./components/CharacterSearch";
 
 function App() {
   return (
-    <div>
-      <h1>AniList Free GraphQl Endpoint </h1>
-      <h3>
-        <a href="https://anilist.co/graphiql">how to use This GrapQL API </a>
-      </h3>
-      <CharacterSearch />
-    </div>
+    <React.Fragment>
+      <div>
+        <AppBar
+          sx={{
+            fontSize: "1.5rem",
+            display: "flex",
+            height: "75px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          AniList Free GraphQl Endpoint
+          <Tooltip
+            title="The result of Search depends on Anilist API.
+              Due to partial matching rather than exact matching, 
+              there is a possibility of unexpected results. "
+            placement="right-start"
+          >
+            <Chip label="About Results" color="warning" />
+          </Tooltip>
+        </AppBar>
+        <div
+          style={{
+            display: "flex",
+            marginTop: "75px",
+            justifyContent: "center",
+          }}
+        >
+          <CharacterSearch />
+        </div>
+        <h4
+          style={{
+            display: "flex",
+            paddingRight: "15px",
+            marginTop: "20px",
+            justifyContent: "center",
+          }}
+        >
+          <a href="https://anilist.co/graphiql">※how to use This GrapQL API </a>
+        </h4>
+      </div>
+    </React.Fragment>
   );
 }
 export default App;
--- src/components/CharacterSearch.tsx
+++ src/components/CharacterSearch.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Chip, Input } from "@mui/material";
+import { Button } from "@mui/material";
+import { Tooltip } from "@mui/material";
 import { useCharacterSearch } from "../hooks/useCharacterSearch";
 
 function CharacterSearch() {
@@ -13,13 +16,15 @@ function CharacterSearch() {
 
   return (
     <div>
-      <input
+      <Input
         type="text"
         value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
-        placeholder="好きなキャラを検索しよう"
+        onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
+          setSearchText(e.target.value)
+        }
+        placeholder="Typing AnimeCharacter"
       />
-      <button onClick={handleSearch}>検索</button>
+      <Button onClick={handleSearch}>Search</Button>
 
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
@@ -27,7 +32,9 @@ function CharacterSearch() {
       {data && data.Character && (
         <div>
           <h2>Name: {data.Character.name.full}</h2>
-          <img src={data.Character.image.medium} alt="Character Image" />
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <img src={data.Character.image.medium} alt="Character Image" />
+          </div>
           <p>
             Date of Birth: {data.Character.dateOfBirth.year}-
             {data.Character.dateOfBirth.month}-{data.Character.dateOfBirth.day}
@@ -39,6 +46,16 @@ function CharacterSearch() {
           >
             More Info
           </a>
+          {/* <div style={{ marginTop: "5px" }}>
+            <Tooltip
+              title="The result of Search depends on Anilist API.
+              Due to partial matching rather than exact matching, 
+              there is a possibility of unexpected results. "
+              placement="right-start"
+            >
+              <Chip label="About Results" color="warning" variant="outlined" />
+            </Tooltip>
+          </div> */}
         </div>
       )}
     </div>
